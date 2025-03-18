0bf629b42d522f6a7a0e9143b08057e5dad71dfa
S.Adithyan <adithyans102002@gmail.com>
2025-03-18 15:17:36 +0530

    staff  hosting11

--- Staff/vite.config.js
+++ Staff/vite.config.js
@@ -11,15 +11,25 @@ export default defineConfig({
   assetsInclude: ['**/*.shard1', '**/*.json'],
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    minify: true,
-    chunkSizeWarningLimit: 1000000000,
+    sourcemap: false,
+    minify: 'esbuild',
+    chunkSizeWarningLimit: 1000,
     commonjsOptions: {
       include: [/node_modules/],
       transformMixedEsModules: true
     },
     rollupOptions: {
       output: {
+        manualChunks: {
+          'mui': ['@mui/material'],
+          'vendor': [
+            'react',
+            'react-dom',
+            'react-router-dom',
+            '@emotion/react',
+            '@emotion/styled'
+          ]
+        },
         assetFileNames: (assetInfo) => {
           if (assetInfo.name.endsWith('.shard1')) {
             return 'assets/models/[name][extname]';
